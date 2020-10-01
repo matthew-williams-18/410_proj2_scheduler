5911a10087fcdaf851c38e99b0bcf44d7df20a29
matthew-williams-18 <matthew.williams.18@cnu.edu>
2020-10-01 14:17:52 -0400

    Implemented dispatcher functions in dispatcher.cpp

--- dispatcher/dispatcher.cpp
+++ dispatcher/dispatcher.cpp
@@ -2,8 +2,31 @@
  * Dispatcher.cpp
  *
  *  Created on: Sep 8, 2019
- *      Author: keith
+ *  Last updated: Oct 1, 2020
+ *      Author: keith, Matt Williams
  */
 
 //TODO fill in content
+#include "../includes/PCB.h"
+#include "../includes/CPU.h"
+#include "../includes/dispatcher.h"
 
+
+//pull current process (if any) off CPU and return it
+//if nothing on CPU returns an uninitialized PCB
+PCB Dispatcher::get_from_CPU(){
+	is_valid_job_on_cpu = false;
+	return cpu->get_process_off_core();
+}
+
+//place the current process on the CPU for execution
+void Dispatcher::put_on_CPU(PCB &process){
+	is_valid_job_on_cpu = true;
+	cpu->put_process_on_core(process);
+}
+
+
+//is CPU idle or working
+bool Dispatcher::isValidJobOnCPU(){
+	return is_valid_job_on_cpu;
+}
